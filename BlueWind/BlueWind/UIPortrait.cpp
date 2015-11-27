b3768acef456e80502dbe5ed1d8e9646f2023783
Joakim Bertils <joabe408@student.liu.se>
2015-11-27 13:02:55 +0100

    Mindre fix av porträtt.

--- BlueWind/BlueWind/UIPortrait.cpp
+++ BlueWind/BlueWind/UIPortrait.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 UIPortrait::UIPortrait(UserInterface * uiPtr, Player * playerPtr)
-	:UIComponent{ sf::Vector2f(10,10), sf::Vector2f(100,120), uiPtr, playerPtr },
-	healthBar_{position_ + sf::Vector2f(10,40), sf::Vector2f(80,20), sf::Color::Red, "HP", uiPtr},
-	manaBar_{ position_ + sf::Vector2f(10,60), sf::Vector2f(80,20), sf::Color::Yellow, "kWh", uiPtr },
-	expBar_{ position_ + sf::Vector2f(10,80), sf::Vector2f(80,20), sf::Color::Green, "XP", uiPtr }
+	:UIComponent{ sf::Vector2f(10,10), sf::Vector2f(200,120), uiPtr, playerPtr },
+	healthBar_{position_ + sf::Vector2f(10,40), sf::Vector2f(180,20), sf::Color::Red, "HP", uiPtr},
+	manaBar_{ position_ + sf::Vector2f(10,60), sf::Vector2f(180,20), sf::Color::Yellow, "kWh", uiPtr },
+	expBar_{ position_ + sf::Vector2f(10,80), sf::Vector2f(180,20), sf::Color::Green, "XP", uiPtr }
 {
 }
 
@@ -26,13 +26,13 @@ void UIPortrait::render(GameWindow & window)
 {
 	sf::RectangleShape frame{ size_ };
 	frame.setPosition(position_);
-	frame.setFillColor(sf::Color{ 200,200,200,255 });
+	frame.setFillColor(sf::Color{ 200,200,200,100 });
 	frame.setOutlineColor(sf::Color{ 100,100,100,255 });
 	frame.setOutlineThickness(4.0f);
 	window.draw(frame);
 
 	sf::Text nameText{ player_->getName() + "  " + to_string(player_->getLevel()), ui_->getFont(), 30 };
-	nameText.setPosition(position_);
+	nameText.setPosition(position_ + sf::Vector2f(10,0));
 	nameText.setColor(sf::Color::Black);
 	window.draw(nameText);
 
