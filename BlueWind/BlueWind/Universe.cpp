949cb9cdf1839bbb191ceb1ecc61cd0fa6d4b462
Joakim Bertils <joabe408@student.liu.se>
2015-11-30 13:40:43 +0100

    Sätt NPC och Fiender i rätt värld.

--- BlueWind/BlueWind/Universe.cpp
+++ BlueWind/BlueWind/Universe.cpp
@@ -101,7 +101,7 @@ void Universe::populateWorlds()
 	//OBS!!!!!!
 	//UNIKA ID KRÄVS
 	addNPC(0, new NPC(1, 100, 10, 0, "Paul", sf::Vector2f(3 * Tile::TILESIZE, 10 * Tile::TILESIZE), getWorld(0), gamePointer_->getTexture("NPC"), "Here comes the dirac train!", PaulInteract, "res/textures/player/player32.png"));
-	addNPC(1, new NPC(1, 100, 10, 1, "Erwin", sf::Vector2f(10 * Tile::TILESIZE, 5 * Tile::TILESIZE), getWorld(0), gamePointer_->getTexture("NPC"), "Hej, jag heter Erwin!", ErwinInteract, "res/textures/player/player32.png"));
+	addNPC(1, new NPC(1, 100, 10, 1, "Erwin", sf::Vector2f(10 * Tile::TILESIZE, 5 * Tile::TILESIZE), getWorld(1), gamePointer_->getTexture("NPC"), "Hej, jag heter Erwin!", ErwinInteract, "res/textures/player/player32.png"));
 	addNPC(0, new NPC(1, 100, 10, 2, "James Clerk", sf::Vector2f(10 * Tile::TILESIZE, 3 * Tile::TILESIZE), getWorld(0), gamePointer_->getTexture("NPC"), "Hej, jag heter James Clerk!", JamesClerkInteract, "res/textures/player/player32.png"));
 	//--------------------------
 	addEnemy(0, new Enemy(1, 100, 10, 0, "Pelle", sf::Vector2f(12 * Tile::TILESIZE, 14 * Tile::TILESIZE), getWorld(0), gamePointer_->getTexture("enemy"), "res/textures/player/enemy32.png"));
