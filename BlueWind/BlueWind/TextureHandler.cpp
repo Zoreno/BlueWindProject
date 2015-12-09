d0f9b63cd73c076709e9d0603c4f1f71f21bdf53
dirac-hatt <fregu856@student.liu.se>
2015-12-09 11:37:25 +0100

    Merge

--- BlueWind/BlueWind/TextureHandler.cpp
+++ BlueWind/BlueWind/TextureHandler.cpp
@@ -31,6 +31,7 @@ TextureHandler::TextureHandler()
 	loadTexture("enemy1", "res/textures/enemy1.png"); 
 	loadTexture("enemy2", "res/textures/enemy2.png");
 	loadTexture("enemy3", "res/textures/enemy3.png");
+	loadTexture("enemy4", "res/textures/enemy4.png");
 	loadTexture("enemy7", "res/textures/enemy7.png");
 	loadTexture("sand", "res/textures/sand.png");
 	loadTexture("palmtree", "res/textures/palmtree.png");
