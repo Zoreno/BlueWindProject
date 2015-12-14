f68370827d01867a71aef1ea05e53318771c165d
dirac-hatt <fregu856@student.liu.se>
2015-12-14 17:22:05 +0100

    FrameButton halvt klar.

--- BlueWind/BlueWind/FrameButton.cpp
+++ BlueWind/BlueWind/FrameButton.cpp
@@ -1,7 +1,65 @@
+/*
+* IDENTIFIERING
+*
+* Filnamn:    FrameButton.cpp
+* Enhetsnamn:
+* Typ:        Definitioner hörande till klassen FrameButton
+* Revision:   1
+* Skriven av: Simon Arkholt, Fredrik Gustafsson
+*
+*
+* BESKRIVNING
+*
+* Denna implementeringsfil definierar medlemsfunktioner för klassen FrameButton.
+*
+* REVISIONSBERÄTTELSE
+*
+* Revision     Datum   Förändringar
+*
+* 1            151214  Ursprungsversion
+*/
+
+/*
+* REFERERADE BIBLIOTEK OCH MODULER
+*/
+
 #include "FrameButton.h"
 #include "Application.h"
 
 using namespace std;
+
+////////// FORTSÄTT HÄR FREDRIK!!
+
+/*
+* KONSTRUKTOR FrameButton(Application* appPtr, sf::Vector2f pos, sf::Vector2f size, std::string normalFile, std::string hoverFile, std::function<void(Application*)> callbackFunc)
+*
+* BESKRIVNING
+*
+* Denna konstruktor skapar ett GameWon-objekt.
+*
+* INDATA
+*
+* appPtr:		Pekare till applikationen som GameWon-objektet är en del av.
+*
+* UTDATA
+*
+* -
+*
+* SIDOEFFEKTER
+*
+* -
+*
+* UTNYTTJAR
+*
+* -
+*
+* REVISIONSBERÄTTELSE
+*
+* Revision             Datum           Förändringar
+*
+* 1                    151214          Ursprungsversion
+*
+*/
 
 FrameButton::FrameButton(Application* appPtr, sf::Vector2f pos, sf::Vector2f size, std::string normalFile, std::string hoverFile, std::function<void(Application*)> callbackFunc)
 	: appPointer_{ appPtr }, 
@@ -9,9 +67,9 @@ FrameButton::FrameButton(Application* appPtr, sf::Vector2f pos, sf::Vector2f siz
 	size_{ size }, 
 	callback_{callbackFunc}
 {
-	if (!normalTexture.loadFromFile(normalFile))
+	if (!normalTexture_.loadFromFile(normalFile))
 		throw FrameButtonException("Kunde inte läsa in textur för knapp från fil:" + normalFile);
-	if (!hoverTexture.loadFromFile(hoverFile))
+	if (!hoverTexture_.loadFromFile(hoverFile))
 		throw FrameButtonException("Kunde inte läsa in textur för knapp från fil:" + hoverFile);
 }
 
@@ -30,9 +88,9 @@ void FrameButton::update()
 	sf::Vector2i mousePosition{ appPointer_->getGameWindow().mapPixelToCoords(sf::Mouse::getPosition(appPointer_->getGameWindow())) };
 		
 	if (mouseOnButton(mousePosition))
-		sprite_.setTexture(hoverTexture);
+		sprite_.setTexture(hoverTexture_);
 	else
-		sprite_.setTexture(normalTexture);	
+		sprite_.setTexture(normalTexture_);	
 }
 
 void FrameButton::render(GameWindow & window)
@@ -52,5 +110,3 @@ bool FrameButton::mouseOnButton(sf::Vector2i mousePosition)
 	return (mousePosition.x > position_.x && mousePosition.x < position_.x + size_.x &&
 			mousePosition.y > position_.y && mousePosition.y < position_.y + size_.y);
 }
-
-
