47ac0999aa7cea24b0aa9de803334257fb461c9d
Johannes Grundell <johgr505@student.liu.se>
2015-12-14 14:42:18 +0100

    Inventory kommenterat.

--- BlueWind/BlueWind/Inventory.h
+++ BlueWind/BlueWind/Inventory.h
@@ -1,11 +1,75 @@
+/*
+* IDENTIFIERING
+*
+* Filnamn:    Inventory.h
+* Enhetsnamn: Inventory
+* Typ:        Moduldeklaration
+* Revision:   1
+* Skriven av: Joakim Bertils, Johannes Grundell
+*
+*
+* BESKRIVNING
+*
+* Denna modul representerar förråd i spelet.
+*
+* REVISIONSBERÄTTELSE
+*
+* Revision     Datum   Förändringar
+*
+* 1            151120  Ursprungsversion
+*
+*/
+
 #pragma once
 
+/*
+* REFERERADE BIBLIOTEK OCH MODULER
+*/
 #include <vector>
-#include "Item.h"
 #include <stdexcept>
+#include "Item.h"
+
+/*
+* Framåtdeklarationer
+*/
 
 class Player;
 class Game;
+
+/*
+* KLASS InvetoryException
+*
+* BASKLASSER
+*
+* logic_error
+*
+* BESKRIVNING
+*
+* Klassen hanterar undantag i Inventory.
+*
+* TILLSTÅND
+*
+* -
+*
+* KONSTRUKTORER
+*
+* default-konstruktor.
+*
+* OPERATIONER
+*
+* -
+*
+* DATAMEDLEMMAR
+*
+* -
+*
+* REVISIONSBERÄTTELSE
+*
+* Revision     Datum   Förändringar
+*
+* 1            151120  Ursprungsversion
+*
+*/
 
 class InventoryException : public std::logic_error
 {
@@ -13,25 +77,63 @@ public:
 	using std::logic_error::logic_error;
 };
 
+/*
+* KLASS Inventory
+*
+* BASKLASSER
+*
+* -
+*
+* BESKRIVNING
+*
+* Klassen representerar förråd i spelet
+*
+* TILLSTÅND
+*
+* -
+*
+* KONSTRUKTORER
+*
+* Inventory(Player* playerPtr, Game* gamePtr);
+*
+* OPERATIONER
+*
+* -
+*
+* DATAMEDLEMMAR
+*
+* items_:			Lista över förrådets föremål.
+* maxSize_:			Förrådets maxstorlek.
+* playerPointer_:	Pekare till spelaren.
+* gamePointer_:		Pekare till spelframe.
+* allItems_:		Lista med alla items som finns i spelet.
+*
+* REVISIONSBERÄTTELSE
+*
+* Revision     Datum   Förändringar
+*
+* 1            151120  Ursprungsversion
+*
+*/
+
 class Inventory
 {
 public:
 	Inventory() = delete;
-	Inventory(Player*, Game*);
 	Inventory(const Inventory&) = delete;
 	Inventory& operator=(const Inventory&) = delete;
+	Inventory(Player*, Game*);
 	~Inventory();
 
 	void addItem(int);
 	void removeItem(int);
 	bool hasItem(int);
 	bool isFull();
-	int getCount(int);
+	const int getCount(int);
+	const int getSize();
+	const unsigned int getMaxSize();
 
 	Item* at(int);
-
-	int getSize();
-	const unsigned int getMaxSize();
 
 	std::string toString();
 
@@ -43,4 +145,8 @@ private:
 
 	void populateItemList();
 	std::vector<Item*> allItems_;
-};
+};//class Inventory
+
+/*
+ * SLUT PÅ FILEN Inventory.h
+ */
