011074000725c4d135115ad7cf84fac31f8e0cb2
dirac-hatt <fregu856@student.liu.se>
2015-12-14 22:54:00 +0100

    GameWindow kommenterad.

--- BlueWind/BlueWind/GameWindow.cpp
+++ BlueWind/BlueWind/GameWindow.cpp
@@ -1,6 +1,67 @@
+/*
+* IDENTIFIERING
+*
+* Filnamn:    GameWindow.cpp
+* Enhetsnamn: GameWindow
+* Typ:        Definitioner hörande till klassen GameWindow
+* Revision:   1
+* Skriven av: Joakim Bertils, Johannes Grundell
+*
+*
+* BESKRIVNING
+*
+* Denna implementeringsfil definierar medlemsfunktioner för klassen GameWindow.
+*
+* REVISIONSBERÄTTELSE
+*
+* Revision     Datum   Förändringar
+*
+* 1            151214  Ursprungsversion
+*/
+
+/*
+* REFERERADE BIBLIOTEK OCH MODULER
+*/
+
 #include "GameWindow.h"
+
+/*
+* KONSTRUKTOR GameWindow::GameWindow(sf::VideoMode videoMode, std::string title)
+*
+* BESKRIVNING
+*
+* Denna konstruktor skapar ett spelfönster.
+*
+* INDATA
+*
+* videoMode: Spelfönstrets storlek. TODO Bättre beskrivning?!
+* title: Spelfönstrets namn.
+*
+* UTDATA
+*
+* -
+*
+* SIDOEFFEKTER
+*
+* -
+*
+* UTNYTTJAR
+*
+* -
+*
+* REVISIONSBERÄTTELSE
+*
+* Revision             Datum           Förändringar
+*
+* 1                    151214          Ursprungsversion
+*
+*/
 
 GameWindow::GameWindow(sf::VideoMode videoMode, std::string title)
 	: sf::RenderWindow(videoMode, title)
 {
 }
+
+/*
+* SLUT PÅ FILEN GameWindow.cpp
+*/
